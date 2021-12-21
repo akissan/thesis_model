0e60f2240fdafc5b810564545ba9e6d180c6cfb9
akissan <astellakissan@gmail.com>
2021-12-21 12:28:32 +0300

    random values

--- src/tools/random.ts
+++ src/tools/random.ts
@@ -1,5 +1,5 @@
-import { exec } from "child_process";
-import { writeFile } from "fs";
+// import { exec } from "child_process";
+// import { writeFile } from "fs";
 
 const getRndNum = () =>
   (Math.random() +
@@ -11,28 +11,28 @@ const getRndNum = () =>
     3) /
   3;
 
-const getTestValues = () => {
-  const vals = [];
-  const N = 5000;
-  for (let i = 0; i < N; i++) {
-    const val = getRndNum();
-    vals.push(val);
-  }
-  vals.sort((a, b) => a - b);
-  writeFile(
-    "/Users/akissan/Dev/thesis_plots/test_0.json",
-    JSON.stringify(vals),
-    () => {
-      console.log("ura!");
-      //   exec("which python", console.log);
-      exec("python3 /Users/akissan/Dev/thesis_plots/test_0.py", console.log);
-    }
-  );
+// const getTestValues = () => {
+//   const vals = [];
+//   const N = 5000;
+//   for (let i = 0; i < N; i++) {
+//     const val = getRndNum();
+//     vals.push(val);
+//   }
+//   vals.sort((a, b) => a - b);
+//   writeFile(
+//     "/Users/akissan/Dev/thesis_plots/test_0.json",
+//     JSON.stringify(vals),
+//     () => {
+//       console.log("ura!");
+//       //   exec("which python", console.log);
+//       exec("python3 /Users/akissan/Dev/thesis_plots/test_0.py", console.log);
+//     }
+//   );
 
-  //   exec(``);
+//   //   exec(``);
 
-  //   console.log(vals);
-  //   console.log(vals.filter((val) => val < 0.125).length / N);
-};
+//   //   console.log(vals);
+//   //   console.log(vals.filter((val) => val < 0.125).length / N);
+// };
 
-getTestValues();
+// getTestValues();
